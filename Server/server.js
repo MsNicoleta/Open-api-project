c1b2a02d1fd942671d20125410cfd2deb5be0768
NG <msnicoleta@outlook.com>
2023-05-26 23:52:12 +0100

    import server links

--- Server/server.js
+++ Server/server.js
@@ -1,3 +1,11 @@
 import express from 'express';
-import * as dotenv from 'cors';
+import * as dotenv from 'dotenv';
+import cors from 'cors';
 import { Configuration, OpenAIApi } from 'openai';
+
+detenv. config();
+
+const Configuration = new Configuration({
+    apiKey: process.env.OPEN_API_KEY,
+})
+const openai = new OpenAIApi(configuration);
