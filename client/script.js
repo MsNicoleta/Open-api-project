b2e2d8bc356cca8e77574b1bec276051dea15842
NG <msnicoleta@outlook.com>
2023-05-26 15:52:10 +0100

    -add the form & link the svg sent file to it

---
+++ client/script.js
@@ -0,0 +1,7 @@
+/* import assets */
+import bot from './assets/bot.svg'
+import user from './assets/user.svg'
+
+/* target the elements manualy by using .querySelector */
+const form = document.querySelector('form'); // was tarheted by tag name as it is the only form in our document.
+const chatContainer = document.querySelector('#chatContainer')
